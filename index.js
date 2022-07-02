737f5b14515883bdcd45afbd6a9b49de39a0b189
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-07-02 17:40:17 +0530

    projectone

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const ProductModel = require("./model/product")
+const connection = require("./db")
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+    const products = await ProductModel.find()
+    res.send(products)
+})
+
+app.post("/products/prod", async (req, res) => {
+    const product = await new ProductModel(req.body)
+    await product.save();
+    res.send(product)
+})
+
+app.listen(process.env.PORT, async () => {
+    try {
+        await connection;
+    }
+    catch {
+        console.log("Something went wrong while connecting to db")
+    }
+    console.log(`listening on ${process.env.PORT}`)
+})
---
+++ model/product.js
@@ -0,0 +1,11 @@
+//schema
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: String,
+    price: Number
+})
+
+const ProductModel = mongoose.model("product", productSchema)
+
+module.exports = ProductModel;
